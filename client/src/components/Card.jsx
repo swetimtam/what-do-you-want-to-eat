f578e5873f28a24aa1db3d23eb16305b0d852eae
Timothy Nguyen <shimoshixp@gmail.com>
2021-03-30 21:58:08 -0700

    Show categories of business

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-const Card = ({ restaurant }) => (
-  <div>
-    <div
-      className="card-img"
-      style={{ backgroundImage: `url(${restaurant.image_url})` }}
-    />
-    <p>Name: { restaurant.name }</p>
-    <p>Rating: { restaurant.rating }</p>
-    <p>Price: { restaurant.price }</p>
-  </div>
-)
+const Card = ({ restaurant }) => {
+  const categories = [];
+
+  for (let i = 0; i < restaurant.categories.length; i++) {
+    categories.push(restaurant.categories[i].title);
+  }
+
+  return (
+    <div className="card">
+      <div
+        className="card-img"
+        style={{ backgroundImage: `url(${restaurant.image_url})` }}
+      />
+      <p>Category: { categories.join(', ') }</p>
+      <p>Name: {restaurant.name}</p>
+      <p>Rating: {restaurant.rating}</p>
+      <p>Price: {restaurant.price}</p>
+    </div>
+  )
+}
 
 export default Card;
