075a6788731e8da29ac0bebbde78414a06260a46
Timothy Nguyen <shimoshixp@gmail.com>
2021-06-21 07:45:27 -0700

    remove from git ignore

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  CLIENT: 'BCSO8BXOZM0KAsmAlyp2TA',
+  API_KEY: 'Bearer 7_ZZgorurpaYyz6yyD-cSmQU7fU6FwXj0gO4Ht9PjOtHinh3Alrm-G7taKyZ2ngVq_7WuNOIT4py1TkcLXvNAciNh55A4tlvKQ58a5OmKKdbRj21aWdtH1aUlXNiYHYx',
+}
