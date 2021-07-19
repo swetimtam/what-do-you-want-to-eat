d772b4b0ae76d4925c20fce5287d76a3d2b0e5e2
Timothy Nguyen <shimoshixp@gmail.com>
2021-07-18 20:02:32 -0700

    replace cards with bootstrap components

--- client/src/components/Tinder.jsx
+++ client/src/components/Tinder.jsx
@@ -1,38 +1,50 @@
 import React from 'react';
-import Card from './Card';
+import Cardy from './Card';
+import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Tinder = ({ businesses, offset, finalists, getNextRestaurant, addRestaurant }) => {
-  if (businesses[offset]) {
+  let business = businesses[offset];
+
+  if (business) {
+    const categories = [];
+
+    for (let i = 0; i < business.categories.length; i++) {
+      categories.push(business.categories[i].title);
+    }
+
     return (
       <>
         <h1>These hot local restaurants want you to eat them out!</h1>
         <h1>Tinder Phase: {8 - finalists.length} more</h1>
-        <div className="tinder">
-          <Card
-            restaurant={businesses[offset]}
-          />
-        </div>
-        <Button
-          variant="outline-success"
-        >
-          Like
-        </Button>
-        <button
-          className="btn btn-outline-failure"
-          onClick={getNextRestaurant}
-        >
-          PASS
-        </button>
-        <button
-          className="btn btn-outline-success"
-          onClick={() => {
-            addRestaurant(businesses[offset]);
-          }}
-        >
-          LIKE
-        </button>
+        <Card style={{ width: '400px' }}>
+          <Card.Img variant="top" src={business.image_url} />
+          <Card.Body>
+            <Card.Title>{business.name}</Card.Title>
+            <Card.Subtitle>
+              {`${business.rating}✰ ${business.price} ${(business.distance / 1609).toFixed(1) + 'mi'}`}
+            </Card.Subtitle>
+            <Card.Text>
+              {categories.join(', ')}
+            </Card.Text>
+            <Button
+              variant="outline-danger"
+              onClick={getNextRestaurant}
+            >
+              PASS
+            </Button>
+            {' '}
+            <Button
+              variant="outline-success"
+              onClick={() => {
+                addRestaurant(businesses[offset]);
+              }}
+            >
+              LIKE
+            </Button>
+          </Card.Body>
+        </Card>
       </>
     )
   } else {
