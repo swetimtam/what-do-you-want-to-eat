800fd217286535d821291e3be6bdbfde956d3689
Timothy Nguyen <shimoshixp@gmail.com>
2021-07-19 01:34:36 -0700

    convert tournament cards to bootstrap

--- client/src/components/Tinder.jsx
+++ client/src/components/Tinder.jsx
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Tinder = ({ businesses, offset, finalists, getNextRestaurant, addRestaurant }) => {
-  let business = businesses[offset];
+  const business = businesses[offset];
 
   if (business) {
     const categories = [];
@@ -19,14 +19,25 @@ const Tinder = ({ businesses, offset, finalists, getNextRestaurant, addRestauran
         <h1>These hot local restaurants want you to eat them out!</h1>
         <h1>Tinder Phase: {8 - finalists.length} more</h1>
         <Card style={{ width: '400px' }}>
-          <Card.Img variant="top" src={business.image_url} />
+          <Card.Img
+            variant="top"
+            src={business.image_url}
+            alt="Card image"
+            style={{
+              height: '400px',
+              objectFit: 'cover',
+            }}
+          />
           <Card.Body>
             <Card.Title>{business.name}</Card.Title>
             <Card.Subtitle>
-              {`${business.rating}✰ ${business.price} ${(business.distance / 1609).toFixed(1) + 'mi'}`}
+              {`★${business.rating} · ${business.price} · ${(business.distance / 1609).toFixed(1) + 'mi'}`}
             </Card.Subtitle>
             <Card.Text>
               {categories.join(', ')}
+            </Card.Text>
+            <Card.Text>
+              {`${business.review_count} reviews`}
             </Card.Text>
             <Button
               variant="outline-danger"
--- client/src/components/Tournament.jsx
+++ client/src/components/Tournament.jsx
@@ -1,37 +1,94 @@
 import React from 'react';
-import Card from './Card';
+import Cardy from './Card';
+import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Tournament = ({ finalists, finalOffset, pickChoice, round }) => {
-  if (finalists[finalOffset + 1]) {
+  const business1 = finalists[finalOffset];
+  const business2 = finalists[finalOffset + 1];
+
+  if (business2) {
+    const categories1 = [];
+    const categories2 = [];
+
+    for (let i = 0; i < business1.categories.length; i++) {
+      categories1.push(business1.categories[i].title);
+    }
+
+    for (let i = 0; i < business2.categories.length; i++) {
+      categories2.push(business2.categories[i].title);
+    }
+
     return (
       <div>
         <h1>Tournament: Round {round}</h1>
         <div className="tournament">
           <div>
-            <Card
-              restaurant={finalists[finalOffset]}
-            />
-            <button
-              className="btn"
-              onClick={() => {
-                pickChoice(finalOffset + 1);
-              }}
-            >
-              CHOOSE
-          </button>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={business1.image_url}
+                alt="Card image"
+                style={{
+                  height: '400px',
+                  objectFit: 'cover',
+                }}
+              />
+              <Card.Body>
+                <Card.Title>{business1.name}</Card.Title>
+                <Card.Subtitle>
+                  {`★${business1.rating} · ${business1.price} · ${(business1.distance / 1609).toFixed(1) + 'mi'}`}
+                </Card.Subtitle>
+                <Card.Text>
+                  {categories1.join(', ')}
+                </Card.Text>
+                <Card.Text>
+                  {`${business1.review_count} reviews`}
+                </Card.Text>
+                <Button
+                  variant="outline-primary"
+                  onClick={() => {
+                    pickChoice(finalOffset + 1);
+                  }}
+                >
+                  SELECT
+                </Button>
+              </Card.Body>
+            </Card>
           </div>
           <div>
-            <Card
-              restaurant={finalists[finalOffset + 1]}
-            />
-            <button
-              onClick={() => {
-                pickChoice(finalOffset);
-              }}
-            >
-              CHOOSE
-            </button>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={business2.image_url}
+                alt="Card image"
+                style={{
+                  height: '400px',
+                  objectFit: 'cover',
+                }}
+              />
+              <Card.Body>
+                <Card.Title>{business2.name}</Card.Title>
+                <Card.Subtitle>
+                  {`★${business2.rating} · ${business2.price} · ${(business2.distance / 1609).toFixed(1) + 'mi'}`}
+                </Card.Subtitle>
+                <Card.Text>
+                  {categories2.join(', ')}
+                </Card.Text>
+                <Card.Text>
+                  {`${business2.review_count} reviews`}
+                </Card.Text>
+                <Button
+                  variant="outline-primary"
+                  onClick={() => {
+                    pickChoice(finalOffset);
+                  }}
+                >
+                  SELECT
+                </Button>
+              </Card.Body>
+            </Card>
           </div>
         </div>
       </div>
